01399ca8ee6a4f7acd3856c4fd67fc6f0c20707d
Daniel Song <57929778+dsong831@users.noreply.github.com>
2024-05-09 20:40:36 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', image_file='static/piano.jpg')
 
 @app.route('/reservation', methods=['GET', 'POST'])
 def reservation():
