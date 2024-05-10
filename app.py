a58f8106466cb0b532889530447abad068d95c7f
Daniel Song <57929778+dsong831@users.noreply.github.com>
2024-05-10 17:18:13 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # CORS 허용 설정
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tuning_center.db'
 db = SQLAlchemy(app)
 
